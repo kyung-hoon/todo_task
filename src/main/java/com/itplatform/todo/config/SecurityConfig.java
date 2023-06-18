84aaad735778be9a45c034d023594793ca21b98b
hooni.min <kyunghoon451@gmail.com>
2023-06-18 13:31:42 +0900

    cors 문제 수정

--- src/main/java/com/itplatform/todo/config/SecurityConfig.java
+++ src/main/java/com/itplatform/todo/config/SecurityConfig.java
@@ -1,16 +1,22 @@
 package com.itplatform.todo.config;
 
 import jakarta.servlet.DispatcherType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeHttpRequests(request -> request
@@ -23,4 +29,8 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+
+
+
 }
